74591eec2e18c057881e8b9cd2a8a6260b0d1686
rahulsainlll <rahulsain.social777@gmail.com>
2024-06-18 19:10:38 +0530

    ..empty

--- models/rantModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const rantSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  message: { type: String, required: true },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Rant', rantSchema);
--- routes/rantRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getRants, addRant } = require('../controllers/rantController');
-const router = express.Router();
-
-router.get('/', getRants);
-router.post('/', addRant);
-
-module.exports = router;
